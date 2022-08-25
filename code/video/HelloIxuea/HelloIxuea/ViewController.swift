7bfc1c061334be5d5c79a92e80be68df83f42159
爱学啊开发 <ixueadev@163.com>
2022-08-26 03:31:10 +0800

    8089 Create project.

---
+++ code/video/HelloIxuea/HelloIxuea/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloIxuea
+//
+//  Created by smile on 2022/8/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

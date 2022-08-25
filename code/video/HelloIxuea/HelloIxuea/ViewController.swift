63cbad95ace9f614f174358f5c22f95b350a8d7f
爱学啊开发 <ixueadev@163.com>
2022-08-26 05:13:58 +0800

    2766 Add button click.

--- code/video/HelloIxuea/HelloIxuea/ViewController.swift
+++ code/video/HelloIxuea/HelloIxuea/ViewController.swift
@@ -13,6 +13,10 @@ class ViewController: UIViewController {
     /// @IBOutlet表示这是一个关联可视化布局中控件的变量
     @IBOutlet weak var titleView: UILabel!
     
+    
+    @IBOutlet weak var buttonView: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,6 +26,8 @@ class ViewController: UIViewController {
         //设置文本
         titleView.text = "我们是爱学啊，这是代码中设置的！"
         
+        //添加点击事件
+        buttonView.addTarget(self, action: #selector(buttonClick2(_:)), for: .touchUpInside)
     }
     
     /// 按钮点击回调事件
@@ -33,6 +39,10 @@ class ViewController: UIViewController {
         print("你还真点击啊！")
         
         sender.setTitle("点击了", for: .normal)
+    }
+    
+    @objc func buttonClick2(_ sender: UIButton) {
+        print("你还真点击啊2222！")
     }
     
 }

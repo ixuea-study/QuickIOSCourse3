0a60ecdfe635a09ceeb954b99aba814ff87152d7
爱学啊开发 <ixueadev@163.com>
2022-08-26 04:24:47 +0800

    2763 Set text in code.

--- code/video/HelloIxuea/HelloIxuea/ViewController.swift
+++ code/video/HelloIxuea/HelloIxuea/ViewController.swift
@@ -9,9 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    /// 关联控件
+    /// @IBOutlet表示这是一个关联可视化布局中控件的变量
+    @IBOutlet weak var titleView: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //获取文本
+        print(titleView.text!)
+        
+        //设置文本
+        titleView.text = "我们是爱学啊，这是代码中设置的！"
+        
     }
 
 

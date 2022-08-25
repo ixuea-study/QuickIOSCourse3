b8a1e7c1eaaa1170e421d94eac37cd4e30c1524a
爱学啊开发 <ixueadev@163.com>
2022-08-26 05:27:56 +0800

    2768 Add second controller.

---
+++ code/video/HelloIxuea/HelloIxuea/Controller/SecondController.swift
@@ -0,0 +1,29 @@
+//
+//  SecondController.swift
+//  HelloIxuea
+//
+//  Created by smile on 2022/8/26.
+//
+
+import UIKit
+
+class SecondController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

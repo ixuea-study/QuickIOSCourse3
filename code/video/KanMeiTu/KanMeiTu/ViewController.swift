06b138cc4a7e5a995daaccb191d1aaac4949511d
爱学啊开发 <ixueadev@163.com>
2022-08-26 08:23:32 +0800

    2780 Add kanmeitu project.

---
+++ code/video/KanMeiTu/KanMeiTu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KanMeiTu
+//
+//  Created by smile on 2022/8/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

9a9c04e55d3fc77ac70a336c903e6853a6c5d363
爱学啊开发 <ixueadev@163.com>
2022-08-26 11:12:01 +0800

    2822 Add logout.

--- code/video/KanMeiTu/KanMeiTu/ViewController.swift
+++ code/video/KanMeiTu/KanMeiTu/ViewController.swift
@@ -14,6 +14,14 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func logoutClick(_ sender: Any) {
+        //清除登录标志
+        PreferenceUtil.setLogin(false)
+        
+        //退出后跳转到登录界面
+        //当然大家可以根据业务逻辑调整
+        SceneDelegate.shared.toLogin()
+    }
+    
 }
 
